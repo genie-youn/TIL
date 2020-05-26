9f488b7d9ee3b66e7a2533b4c1941b3efe146dcc
genie-youn <yjs930915@gmail.com>
2020-05-27 08:58:23 +0900

    javascript focusout과 click에 대하여

---
+++ JavaScript/javascript_focusout과_click에_대하여/demo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <ul>
+        <li @click="click">추천된 태그1</li>
+        <li @click="click">추천된 태그2</li>
+      </ul>
+    </div>
+    <input @focusout="focuseout" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      insertedTag: "",
+    }
+  },
+  methods: {
+    focuseout() {
+      // focuseout 됐을때 처리해야할 로직들..
+      console.log("focusout");
+    },
+    click() {
+      // click 됐을때 처리해야할 로직들
+      console.log("click");
+    }
+  },
+}
+</script>
